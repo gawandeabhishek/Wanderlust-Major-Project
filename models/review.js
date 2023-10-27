7c2a032ade7ffc56b2ac8fa2e513d969e551d428
Jayashree Gawande <jgawande777@gmail.com>
2023-10-27 22:14:24 +0530

    Add Project Files

---
+++ models/review.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    comment: String,
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ],
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
